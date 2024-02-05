1ae7b5f0ac327cc2123e1635d9c21e53d62abfdd
trena <20079502+tholst952@users.noreply.github.com>
2024-02-05 10:33:48 -0800

    map markers are clearing when workout is deleted, but now we have different errors to solve

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # mapty-challenge
+
+Features I am currently trying to add (2/5/24):
+
+- Delete a workout from the sidebar and have the corresponding marker disappear from the map along with the data from local storage.
+
+- Implement a button to delete all workouts, markers and data from local storage.
---
+++ script.js
@@ -0,0 +1,382 @@
+'use strict';
+
+class Workout {
+  date = new Date();
+  id = Date.now().toString().slice(-10);
+  clicks = 0;
+
+  constructor(coords, distance, duration) {
+    this.coords = coords; // [lat, lng]
+    this.distance = distance; // in km
+    this.duration = duration; // in min
+  }
+
+  _setDescription() {
+    // prettier-ignore
+    const months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+
+    this.description = `${this.type[0].toUpperCase()}${this.type.slice(1)} on ${
+      months[this.date.getMonth()]
+    } ${this.date.getDate()}`;
+  }
+
+  click() {
+    this.clicks++;
+  }
+}
+
+class Running extends Workout {
+  type = 'running';
+
+  constructor(coords, distance, duration, cadence) {
+    super(coords, distance, duration);
+    this.cadence = cadence;
+    this.calcPace();
+    this._setDescription();
+  }
+
+  calcPace() {
+    this.pace = this.duration / this.distance; // min/km
+    return this.pace;
+  }
+}
+
+class Cycling extends Workout {
+  type = 'cycling';
+
+  constructor(coords, distance, duration, elevationGain) {
+    super(coords, distance, duration);
+    this.elevationGain = elevationGain;
+    this.calcSpeed();
+    this._setDescription();
+  }
+
+  calcSpeed() {
+    // km divided by hour
+    this.speed = this.distance / (this.duration / 60);
+    return this.speed;
+  }
+}
+
+////////////////////////////////////////////////////////
+// Application Architecture
+const form = document.querySelector('.form');
+const containerWorkouts = document.querySelector('.workouts');
+const inputType = document.querySelector('.form__input--type');
+const inputDistance = document.querySelector('.form__input--distance');
+const inputDuration = document.querySelector('.form__input--duration');
+const inputCadence = document.querySelector('.form__input--cadence');
+const inputElevation = document.querySelector('.form__input--elevation');
+const closeBtn = document.querySelector('.close-btn');
+const sidebar = document.querySelector('.sidebar');
+const deleteAllBtn = document.querySelector('.delete-all-btn');
+
+class App {
+  #map;
+  #mapZoomLevel = 14;
+  #mapEvent;
+  #workouts = [];
+  #markers = [];
+
+  constructor() {
+    // Get user's position
+    this._getPosition();
+
+    // Get data from local storage
+    this._getLocalStorage();
+
+    // Attach event handlers
+    form.addEventListener('submit', this._newWorkout.bind(this));
+    inputType.addEventListener('change', this._toggleElevationField);
+    containerWorkouts.addEventListener('click', this._moveToPopup.bind(this));
+    containerWorkouts.addEventListener('click', this._deleteWorkout.bind(this));
+    sidebar.addEventListener('click', this._deleteAllWorkouts.bind(this));
+  }
+
+  _getPosition() {
+    if (navigator.geolocation)
+      navigator.geolocation.getCurrentPosition(
+        this._loadMap.bind(this),
+        function () {
+          alert('Could not get your position');
+        },
+        { enableHighAccuracy: false }
+      );
+  }
+
+  _loadMap(position) {
+    const { latitude, longitude } = position.coords;
+    const coords = [latitude, longitude];
+
+    this.#map = L.map('map').setView(coords, this.#mapZoomLevel);
+
+    L.tileLayer('https://tile.openstreetmap.fr/hot/{z}/{x}/{y}.png', {
+      attribution:
+        '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+    }).addTo(this.#map);
+
+    // Handling clicks on map
+    this.#map.on('click', this._showForm.bind(this));
+
+    // Render the markers on map once map is ready
+    this.#workouts.forEach(work => {
+      this._renderWorkoutMarker(work);
+    });
+  }
+
+  _showForm(mapE) {
+    this.#mapEvent = mapE;
+    form.classList.remove('hidden');
+    inputDistance.focus();
+  }
+
+  _hideForm() {
+    // Empty the input fields
+    // prettier-ignore
+    inputDistance.value = inputDuration.value = inputDuration.value = inputElevation.value = '';
+    form.style.display = 'none';
+    form.classList.add('hidden');
+    setTimeout(() => (form.style.display = 'grid'), 1000);
+  }
+
+  _toggleElevationField() {
+    inputElevation.closest('.form__row').classList.toggle('form__row--hidden');
+    inputCadence.closest('.form__row').classList.toggle('form__row--hidden');
+  }
+
+  _newWorkout(e) {
+    // Helper function checks if all inputs are valid ✔
+    const validInputs = (...inputs) =>
+      inputs.every(inp => Number.isFinite(inp));
+
+    const allPositive = (...inputs) => inputs.every(inp => inp > 0);
+
+    e.preventDefault();
+
+    //Get data from form
+    const type = inputType.value;
+    const distance = +inputDistance.value;
+    const duration = +inputDuration.value;
+    const { lat, lng } = this.#mapEvent.latlng;
+    let workout;
+
+    //If workout is running, create running object
+    if (type === 'running') {
+      const cadence = +inputCadence.value;
+
+      // Check if data is valid
+      if (
+        // !Number.isFinite(distance) ||
+        // !Number.isFinite(duration) ||
+        // !Number.isFinite(cadence)
+        !validInputs(distance, duration, cadence) ||
+        !allPositive(distance, duration, cadence)
+      )
+        return alert('Inputs have to be positive numbers!');
+      workout = new Running([lat, lng], distance, duration, cadence);
+    }
+
+    //If workout is cycling, create cycling object
+    if (type === 'cycling') {
+      const elevation = +inputElevation.value;
+
+      if (
+        !validInputs(distance, duration, elevation) ||
+        !allPositive(distance, duration)
+      )
+        return alert('Inputs have to be positive numbers!');
+      workout = new Cycling([lat, lng], distance, duration, elevation);
+    }
+
+    //Add new object to workout array
+    this.#workouts.push(workout);
+
+    //Render workout on map as marker
+    this._renderWorkoutMarker(workout);
+
+    //Render workout on list
+    this._renderWorkout(workout);
+
+    //Hide form & clear input fields
+    this._hideForm();
+
+    //Set local storage to all workouts
+    this._setLocalStorage();
+
+    console.log(workout.id);
+  }
+
+  _renderWorkoutMarker(workout) {
+    const marker = L.marker(workout.coords, { id: workout.id })
+      .addTo(this.#map)
+      .bindPopup(
+        L.popup({
+          maxWidth: 250,
+          minWidth: 100,
+          autoClose: false,
+          closeOnClick: false,
+          className: `${workout.type}-popup`,
+        })
+      )
+      .setPopupContent(
+        `${workout.type === 'running' ? '🏃‍♂️' : '🚴‍♀️'} ${workout.description}`
+      )
+      .openPopup();
+
+    this.#markers.push(marker);
+    console.log(this.#markers);
+  }
+
+  _renderWorkout(workout) {
+    let html = `
+      <li class="workout workout--${workout.type}" id="${workout.id}">
+      
+        <h2 class="workout__title">${workout.description}</h2>
+
+        <svg
+        xmlns="http://www.w3.org/2000/svg"
+        fill="none"
+        viewBox="0 0 24 24"
+        stroke-width="1.5"
+        stroke="currentColor"
+        class="close-btn"
+        data-id="${workout.id}"
+  "
+      >
+        <path
+          stroke-linecap="round"
+          stroke-linejoin="round"
+          d="M6 18 18 6M6 6l12 12"
+        />
+      </svg>
+
+        <div class="workout__details">
+          <span class="workout__icon">${
+            workout.type === 'running' ? '🏃‍♂️' : '🚴‍♀️'
+          }</span>
+          <span class="workout__value">${workout.distance}</span>
+          <span class="workout__unit">km</span>
+        </div>
+        
+        <div class="workout__details">
+          <span class="workout__icon">⏱</span>
+          <span class="workout__value">${workout.duration}</span>
+          <span class="workout__unit">min</span>
+        </div>
+        
+        <div class="workout__details">
+          <span class="workout__icon">⚡️</span>
+          <span class="workout__value">${
+            workout.type === 'running'
+              ? workout.pace.toFixed(1)
+              : workout.speed.toFixed(1)
+          }</span>
+          <span class="workout__unit">${
+            workout.type === 'running' ? 'min/km' : 'km/h'
+          }</span>
+        </div>
+        
+        <div class="workout__details">
+          <span class="workout__icon">${
+            workout.type === 'running' ? '🦶🏼' : '⛰'
+          }</span>
+          <span class="workout__value">${
+            workout.type === 'running' ? workout.cadence : workout.elevationGain
+          }</span>
+          <span class="workout__unit">${
+            workout.type === 'running' ? 'spm' : 'm'
+          }</span>
+        </div>
+      </li>
+    `;
+
+    form.insertAdjacentHTML('afterend', html);
+    this._showDeleteAll();
+  }
+
+  _showDeleteAll() {
+    if (this.#workouts.length >= 2) deleteAllBtn.classList.remove('hidden');
+  }
+
+  //🔥🔥🔥🔥🔥🔥 CHALLENGE to delete a workout 🔥🔥🔥🔥🔥🔥🔥🔥🔥
+  _deleteWorkout(e) {
+    const close = e.target.closest('.close-btn');
+    const workoutElement = e.target.closest('.workout');
+    const workoutId = close.dataset.id;
+
+    // if the clicking happens anywhere other than the button, ignore it
+    if (!close) return; // guard clause
+
+    workoutElement.style.display = 'none';
+
+    // Remove element from sidebar
+    const removeWorkout = this.#workouts.findIndex(
+      workout => workout.id === close.dataset.id
+    );
+
+    // Check if the workout is found before attempting to remove it
+    if (removeWorkout !== -1) {
+      // -1 = element not found
+      this.#workouts.splice(removeWorkout, 1);
+    }
+
+    const marker = this.#markers.find(m => m.options.id === workoutId);
+    this.#map.removeLayer(marker);
+
+    ////////////////////////////////////
+  }
+
+  //🔥🔥🔥🔥🔥🔥 CHALLENGE to delete ALL workouts 🔥🔥🔥🔥🔥🔥🔥🔥🔥
+  _deleteAllWorkouts(e) {
+    const deleteAll = e.target.closest('.delete-all-btn');
+
+    if (!deleteAll) return;
+    console.log('You clicked on the button alright.');
+
+    containerWorkouts.style.display = 'none';
+    deleteAllBtn.classList.add('hidden');
+    localStorage.removeItem('workouts');
+    this.reset();
+  }
+
+  _moveToPopup(e) {
+    const workoutEl = e.target.closest('.workout');
+    if (!workoutEl) return;
+    const workout = this.#workouts.find(
+      work => work.id === workoutEl.dataset.id
+    );
+
+    this.#map.setView(workout.coords, this.#mapZoomLevel, {
+      animate: true,
+      pan: {
+        duration: 1,
+      },
+    });
+
+    // Using the public interface
+    // workout.click();
+  }
+
+  _setLocalStorage() {
+    localStorage.setItem('workouts', JSON.stringify(this.#workouts));
+  }
+
+  _getLocalStorage() {
+    const data = JSON.parse(localStorage.getItem('workouts'));
+
+    if (!data) return; // guard clause
+    this.#workouts = data;
+
+    // Render the workouts in the sidebar
+    this.#workouts.forEach(work => {
+      this._renderWorkout(work);
+    });
+  }
+
+  reset() {
+    localStorage.removeItem('workouts');
+    location.reload();
+  }
+}
+
+const app = new App();
